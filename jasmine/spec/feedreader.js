7b119f52e2d9f79eff961758e631b2767c152422
Catherine Lin <catherine.lin.93@gmail.com>
2017-11-28 11:10:31 -0800

    Attempts to do 8) of the project

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,11 +32,20 @@ $(function() {
          * and that the URL is not empty.
          */
 
+         it('url is not empty', function() {
+            for(var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url)toBeDefined();
+            }
+            
+         });
+
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+
     });
 
 
